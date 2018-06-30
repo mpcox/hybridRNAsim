df67123b6a4d6a251485bebd5bb5e8eafb9c380b
Murray Cox <murray.p.cox@gmail.com>
2018-06-30 10:40:35 +0200

    Improved code commenting

--- hybridRNAsim.R
+++ hybridRNAsim.R
@@ -26,31 +26,24 @@ h.n.diffexp <- 50
 h.seqdepth <- 2.5e7
 h.fraction.upregulated <- 0.5
 
-
+# setup dataframe
 expression.res <- as.data.frame(matrix(0, ncol=26, nrow=0))
 colnames(expression.res) <- c(
 	"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h.nreads", "h.de", "h.frac.up",
 	"o.up", "o.down", "h.up", "h.down", "o.up.fc", "o.down.fc", "h.up.fc", "h.down.fc",
 	"I", "L", "S", "R", "U", "I.fc", "L.fc", "S.fc", "R.fc", "U.fc")
 
-
+# example run
 x <- runsims(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, 
 	o.fraction.upregulated, h.n.diffexp, h.seqdepth, h.fraction.upregulated)
 	
 expression.res <- rbind(expression.res, x)
 expression.res
 
-
+# appended example run
 x <- runsims(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, 
 	o.fraction.upregulated, h.n.diffexp, h.seqdepth, h.fraction.upregulated)
 	
 expression.res <- rbind(expression.res, x)
 expression.res
-
-
-expression.res
-
-
-
-	
 	
