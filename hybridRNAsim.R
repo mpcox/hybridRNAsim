784bd470be7b3c3500762582aaa4ac396aed1d87
Murray Cox <murray.p.cox@gmail.com>
2018-07-02 09:19:04 +0200

    Changed code to allow two hybrids

--- hybridRNAsim.R
+++ hybridRNAsim.R
@@ -4,6 +4,8 @@
 # o.de is the actual number of DE genes, while o.up + o.down are the observed number of DE genes
 # these values are not comparable if o.up + o.down must be signficant *and* 2-fold different
 
+# setwd("/Users/work/Documents/Programmes/R/hybridRNAsim")
+
 # load libraries
 library(compcodeR)
 
@@ -11,26 +13,23 @@ library(compcodeR)
 source("hybridRNAsim_functions.R")
 
 # variables
-iterations				<- 5
-n.genes					<- 1000
-scalar					<- 10
-bio.reps				<- 2
-		
-significance			<- 0.05
-fold.change				<- 2
-
-o.n.diffexp				<- 50
-o.seqdepth				<- 2.5e7
-o.fraction.upregulated 	<- 0.5
-
-h1.n.diffexp			<- 50
-h1.seqdepth				<- 2.5e7
-h1.fraction.upregulated	<- 0.5
-
-second.hybrid			<- FALSE
-h2.n.diffexp			<- 50
-h2.seqdepth				<- 2.5e7
-h2.fraction.upregulated	<- 0.5
+iterations					<- 5
+n.genes						<- 1000
+scalar						<- 10
+bio.reps					<- 2
+			
+significance				<- 0.05
+fold.change					<- 2
+	
+o.n.diffexp					<- 50
+o.seqdepth					<- 2.5e7
+o.fraction.upregulated 		<- 0.5
+	
+h1.n.diffexp				<- 50
+h1.seqdepth					<- 2.5e7
+h1.fraction.upregulated		<- 0.5
+	
+second.hybrid				<- FALSE
 
 
 # set up dataframe
@@ -60,6 +59,49 @@ expression.res
 # appended example run (one hybrid only)
 x <- runsims(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, 
 	o.fraction.upregulated, h1.n.diffexp, h1.seqdepth, h1.fraction.upregulated)
+
+expression.res <- rbind(expression.res, x)
+expression.res
+
+rm(expression.res)
+
+
+# two hybrid example
+
+source("hybridRNAsim_functions.R")
+
+second.hybrid				<- TRUE
+h2.n.diffexp				<- 50
+h2.seqdepth					<- 2.5e7
+h2.fraction.upregulated		<- 0.5
+
+# set up dataframe
+if(second.hybrid == FALSE){
+	expression.res <- as.data.frame(matrix(0, ncol=26, nrow=0))
+	colnames(expression.res) <- c(
+	"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h1.nreads", "h1.de", "h1.frac.up",
+	"o.up", "o.down", "h1.up", "h1.down", "o.up.fc", "o.down.fc", "h1.up.fc", "h1.down.fc",
+	"I1", "L1", "S1", "R1", "U1", "I1.fc", "L1.fc", "S1.fc", "R1.fc", "U1.fc")
+}else if(second.hybrid == TRUE){
+	expression.res <- as.data.frame(matrix(0, ncol=43, nrow=0))
+	colnames(expression.res) <- c(
+	"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h1.nreads", "h1.de", "h1.frac.up", 
+	"h2.nreads", "h2.de", "h2.frac.up", "o.up", "o.down", "h1.up", "h1.down", "h2.up", 
+	"h2.down", "o.up.fc", "o.down.fc", "h1.up.fc", "h1.down.fc", "h2.up.fc", "h2.down.fc",
+	"I1", "L1", "S1", "R1", "U1", "I1.fc", "L1.fc", "S1.fc", "R1.fc", "U1.fc",
+	"I2", "L2", "S2", "R2", "U2", "I2.fc", "L2.fc", "S2.fc", "R2.fc", "U2.fc")
+}
+
+# example run (two hybrids)
+x <- runsims(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, 
+	o.fraction.upregulated, h1.n.diffexp, h1.seqdepth, h1.fraction.upregulated, second.hybrid, h2.n.diffexp, h2.seqdepth, h2.fraction.upregulated)
+
+expression.res <- rbind(expression.res, x)
+expression.res
+
+# appended example run (two hybrids)
+x <- runsims(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, 
+	o.fraction.upregulated, h1.n.diffexp, h1.seqdepth, h1.fraction.upregulated, second.hybrid, h2.n.diffexp, h2.seqdepth, h2.fraction.upregulated)
 
 expression.res <- rbind(expression.res, x)
 expression.res
--- hybridRNAsim_functions.R
+++ hybridRNAsim_functions.R
@@ -34,18 +34,39 @@ count <- function(vector){
 }
 
 # wrapper script for running simulations and analyses
-runsims <- function(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, o.fraction.upregulated, h.n.diffexp, h.seqdepth, h.fraction.upregulated){
+runsims <- function(iterations, n.genes, scalar, bio.reps, significance, fold.change, o.n.diffexp, o.seqdepth, o.fraction.upregulated, h1.n.diffexp, h1.seqdepth, h1.fraction.upregulated, second.hybrid=FALSE, h2.n.diffexp=NULL, h2.seqdepth=NULL, h2.fraction.upregulated=NULL){
+	
+	# check for second hybrid
+	if(second.hybrid == TRUE){
+		if( is.null(h2.n.diffexp) || is.null(h2.seqdepth) || is.null(h2.fraction.upregulated) ){
+			stop("Must define values for h2.n.diffexp, h2.seqdepth and h2.fraction.upregulated")
+		}
+	}
 	
 	# make data frame
-	this.res <- as.data.frame(matrix(0, ncol=26, nrow=0))
-	colnames(this.res) <- c(
-		"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h.nreads", "h.de", "h.frac.up",
-		"o.up", "o.down", "h.up", "h.down", "o.up.fc", "o.down.fc", "h.up.fc", "h.down.fc",
-		"I", "L", "S", "R", "U", "I.fc", "L.fc", "S.fc", "R.fc", "U.fc")
+	if(second.hybrid == FALSE){
+		this.res <- as.data.frame(matrix(0, ncol=26, nrow=0))
+		colnames(this.res) <- c(
+		"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h1.nreads", "h1.de", "h1.frac.up",
+		"o.up", "o.down", "h1.up", "h1.down", "o.up.fc", "o.down.fc", "h1.up.fc", "h1.down.fc",
+		"I1", "L1", "S1", "R1", "U1", "I1.fc", "L1.fc", "S1.fc", "R1.fc", "U1.fc")
+	}else if(second.hybrid == TRUE){
+		this.res <- as.data.frame(matrix(0, ncol=43, nrow=0))
+		colnames(this.res) <- c(
+		"ngenes", "nreps", "o.nreads", "o.de", "o.frac.up", "h1.nreads", "h1.de", "h1.frac.up", 
+		"h2.nreads", "h2.de", "h2.frac.up", "o.up", "o.down", "h1.up", "h1.down", "h2.up", 
+		"h2.down", "o.up.fc", "o.down.fc", "h1.up.fc", "h1.down.fc", "h2.up.fc", "h2.down.fc",
+		"I1", "L1", "S1", "R1", "U1", "I1.fc", "L1.fc", "S1.fc", "R1.fc", "U1.fc",
+		"I2", "L2", "S2", "R2", "U2", "I2.fc", "L2.fc", "S2.fc", "R2.fc", "U2.fc")
+	}
 	
 	# record simulation parameters
-	conditions <- c(n.genes, bio.reps, o.seqdepth, o.n.diffexp, o.fraction.upregulated, h.seqdepth, h.n.diffexp, h.fraction.upregulated)
-	
+	if(second.hybrid == FALSE){
+		conditions <- c(n.genes, bio.reps, o.seqdepth, o.n.diffexp, o.fraction.upregulated, h1.seqdepth, h1.n.diffexp, h1.fraction.upregulated)
+	}else if(second.hybrid == TRUE){
+		conditions <- c(n.genes, bio.reps, o.seqdepth, o.n.diffexp, o.fraction.upregulated, h1.seqdepth, h1.n.diffexp, h1.fraction.upregulated, h2.seqdepth, h2.n.diffexp, h2.fraction.upregulated)
+	}
+
 	# simulate orthologs
 	o.sim <- generateSyntheticData(	dataset = "o.sim", 
 									n.vars = n.genes * scalar,
@@ -63,84 +84,159 @@ runsims <- function(iterations, n.genes, scalar, bio.reps, significance, fold.ch
 	o.sim.dat <- readRDS("o.sim_DESeq2.rds")
 	
 	# simulate homeologs
-	h.sim <- generateSyntheticData(	dataset = "h.sim", 
+	h1.sim <- generateSyntheticData(dataset = "h1.sim", 
 									n.vars = n.genes * scalar,
 									samples.per.cond = bio.reps,
-									n.diffexp = h.n.diffexp * scalar,
-									seqdepth = h.seqdepth * scalar,
-									fraction.upregulated = h.fraction.upregulated,
-									output.file = "h.sim.rds")
+									n.diffexp = h1.n.diffexp * scalar,
+									seqdepth = h1.seqdepth * scalar,
+									fraction.upregulated = h1.fraction.upregulated,
+									output.file = "h1.sim.rds")
 	
-	runDiffExp(data.file = "h.sim.rds", result.extent = "DESeq2",
+	runDiffExp(data.file = "h1.sim.rds", result.extent = "DESeq2",
 	               Rmdfunction = "DESeq2.createRmd",
 	               output.directory = ".", fit.type = "parametric",
 	               test = "Wald")
 	
-	h.sim.dat <- readRDS("h.sim_DESeq2.rds")
+	h1.sim.dat <- readRDS("h1.sim_DESeq2.rds")
+	
+	# simulate homeologs for second hybrid
+	if(second.hybrid == TRUE){
+		h2.sim <- generateSyntheticData(dataset = "h2.sim", 
+										n.vars = n.genes * scalar,
+										samples.per.cond = bio.reps,
+										n.diffexp = h2.n.diffexp * scalar,
+										seqdepth = h2.seqdepth * scalar,
+										fraction.upregulated = h2.fraction.upregulated,
+										output.file = "h2.sim.rds")
+		
+		runDiffExp(data.file = "h2.sim.rds", result.extent = "DESeq2",
+		               Rmdfunction = "DESeq2.createRmd",
+		               output.directory = ".", fit.type = "parametric",
+		               test = "Wald")
+		
+		h2.sim.dat <- readRDS("h2.sim_DESeq2.rds")		
+	}
 	
 	for( i in 1:iterations ){
 		
 		# make subsampled dataset (note: datasets can differ in size by 1)
-		o.values <- sample(1:length(o.sim.dat@result.table[,1]), n.genes, replace=F)
-		h.values <- sample(1:length(h.sim.dat@result.table[,1]), n.genes, replace=F)
+		o.values  <- sample(1:length(o.sim.dat@result.table[,1]),  n.genes, replace=F)
+		h1.values <- sample(1:length(h1.sim.dat@result.table[,1]), n.genes, replace=F)
 		
-		this.o.sim.dat <- o.sim.dat@result.table[o.values,]
-		this.h.sim.dat <- h.sim.dat@result.table[h.values,]
+		this.o.sim.dat  <- o.sim.dat@result.table[o.values,]
+		this.h1.sim.dat <- h1.sim.dat@result.table[h1.values,]
+		
+		if(second.hybrid == TRUE){
+			h2.values <- sample(1:length(h2.sim.dat@result.table[,1]), n.genes, replace=F)
+			this.h2.sim.dat <- h2.sim.dat@result.table[h2.values,]
+		}
 		
 		# determine differential expression (statistically significant)
 		o.sign.up   <- which( this.o.sim.dat$adjpvalue <= significance & this.o.sim.dat$logFC >= 0 )
 		o.sign.down <- which( this.o.sim.dat$adjpvalue <= significance & this.o.sim.dat$logFC < 0 )
 		o.nonsign   <- setdiff(1:n.genes, c(o.sign.up, o.sign.down))
 		
-		h.sign.up   <- which( this.h.sim.dat$adjpvalue <= significance & this.h.sim.dat$logFC >= 0 )
-		h.sign.down <- which( this.h.sim.dat$adjpvalue <= significance & this.h.sim.dat$logFC < 0 )
-		h.nonsign   <- setdiff(1:n.genes, c(h.sign.up, h.sign.down))
-	
-		sim.nde <- c(length(o.sign.up), length(o.sign.down), length(h.sign.up), length(h.sign.down))
+		h1.sign.up   <- which( this.h1.sim.dat$adjpvalue <= significance & this.h1.sim.dat$logFC >= 0 )
+		h1.sign.down <- which( this.h1.sim.dat$adjpvalue <= significance & this.h1.sim.dat$logFC < 0 )
+		h1.nonsign   <- setdiff(1:n.genes, c(h1.sign.up, h1.sign.down))
+		
+		if(second.hybrid == TRUE){
+			h2.sign.up   <- which( this.h2.sim.dat$adjpvalue <= significance & this.h2.sim.dat$logFC >= 0 )
+			h2.sign.down <- which( this.h2.sim.dat$adjpvalue <= significance & this.h2.sim.dat$logFC < 0 )
+			h2.nonsign   <- setdiff(1:n.genes, c(h2.sign.up, h2.sign.down))
+		}
+		
+		if(second.hybrid == FALSE){
+			sim.nde <- c(length(o.sign.up), length(o.sign.down), length(h1.sign.up), length(h1.sign.down))
+		}else if(second.hybrid == TRUE){
+			sim.nde <- c(length(o.sign.up), length(o.sign.down), length(h1.sign.up), length(h1.sign.down), length(h2.sign.up), length(h2.sign.down))
+		}
 	
 		# determine differential expression (statistically significant and >=x-fold change)	
 		o.sign.up.fc   <- which( this.o.sim.dat$adjpvalue <= significance & this.o.sim.dat$logFC >= log2(fold.change) )
 		o.sign.down.fc <- which( this.o.sim.dat$adjpvalue <= significance & this.o.sim.dat$logFC <= -log2(fold.change) )
 		o.nonsign.fc   <- setdiff(1:n.genes, c(o.sign.up.fc, o.sign.down.fc))
 		
-		h.sign.up.fc   <- which( this.h.sim.dat$adjpvalue <= significance & this.h.sim.dat$logFC >= log2(fold.change) )
-		h.sign.down.fc <- which( this.h.sim.dat$adjpvalue <= significance & this.h.sim.dat$logFC <= -log2(fold.change) )
-		h.nonsign.fc   <- setdiff(1:n.genes, c(h.sign.up.fc, h.sign.down.fc))
+		h1.sign.up.fc   <- which( this.h1.sim.dat$adjpvalue <= significance & this.h1.sim.dat$logFC >= log2(fold.change) )
+		h1.sign.down.fc <- which( this.h1.sim.dat$adjpvalue <= significance & this.h1.sim.dat$logFC <= -log2(fold.change) )
+		h1.nonsign.fc   <- setdiff(1:n.genes, c(h1.sign.up.fc, h1.sign.down.fc))
+
+		if(second.hybrid == TRUE){
+			h2.sign.up.fc   <- which( this.h2.sim.dat$adjpvalue <= significance & this.h2.sim.dat$logFC >= log2(fold.change) )
+			h2.sign.down.fc <- which( this.h2.sim.dat$adjpvalue <= significance & this.h2.sim.dat$logFC <= -log2(fold.change) )
+			h2.nonsign.fc   <- setdiff(1:n.genes, c(h2.sign.up.fc, h2.sign.down.fc))
+		}
 		
-		sim.nde.fc <- c(length(o.sign.up.fc), length(o.sign.down.fc), length(h.sign.up.fc), length(h.sign.down.fc))
+		if(second.hybrid == FALSE){
+			sim.nde.fc <- c(length(o.sign.up.fc), length(o.sign.down.fc), length(h1.sign.up.fc), length(h1.sign.down.fc))
+		}else if(second.hybrid == TRUE){
+			sim.nde.fc <- c(length(o.sign.up.fc), length(o.sign.down.fc), length(h1.sign.up.fc), length(h1.sign.down.fc), length(h2.sign.up.fc), length(h2.sign.down.fc))
+		}
 		
 		# build change vectors (statistically significant)
 		orthologs <- vector(length=n.genes)
 		orthologs[o.sign.up]   <- +1
 		orthologs[o.sign.down] <- -1
-		orthologs[o.nonsign]  <- 0
+		orthologs[o.nonsign]   <- 0
 		
-		homeologs <- vector(length=n.genes)
-		homeologs[h.sign.up]   <- +1
-		homeologs[h.sign.down] <- -1
-		homeologs[h.nonsign]  <- 0
+		homeologs1 <- vector(length=n.genes)
+		homeologs1[h1.sign.up]   <- +1
+		homeologs1[h1.sign.down] <- -1
+		homeologs1[h1.nonsign]   <- 0
 		
-		simulation <- matrix(data=cbind(orthologs, homeologs), ncol=2)
-	
+		if(second.hybrid == TRUE){
+			homeologs2 <- vector(length=n.genes)
+			homeologs2[h2.sign.up]   <- +1
+			homeologs2[h2.sign.down] <- -1
+			homeologs2[h2.nonsign]   <- 0			
+		}
+		
+		simulation1 <- matrix(data=cbind(orthologs, homeologs1), ncol=2)
+		
+		if(second.hybrid == TRUE){
+			simulation2 <- matrix(data=cbind(orthologs, homeologs2), ncol=2)
+		}
+		
 		# build change vectors (statistically significant and >=x-fold change)	
 		orthologs.fc <- vector(length=n.genes)
 		orthologs.fc[o.sign.up.fc]   <- +1
 		orthologs.fc[o.sign.down.fc] <- -1
 		orthologs.fc[o.nonsign.fc]  <- 0
 		
-		homeologs.fc <- vector(length=n.genes)
-		homeologs.fc[h.sign.up.fc]   <- +1
-		homeologs.fc[h.sign.down.fc] <- -1
-		homeologs.fc[h.nonsign.fc]  <- 0
+		homeologs1.fc <- vector(length=n.genes)
+		homeologs1.fc[h1.sign.up.fc]   <- +1
+		homeologs1.fc[h1.sign.down.fc] <- -1
+		homeologs1.fc[h1.nonsign.fc]  <- 0
 		
-		simulation.fc <- matrix(data=cbind(orthologs.fc, homeologs.fc), ncol=2)
+		if(second.hybrid == TRUE){
+			homeologs2.fc <- vector(length=n.genes)
+			homeologs2.fc[h2.sign.up.fc]   <- +1
+			homeologs2.fc[h2.sign.down.fc] <- -1
+			homeologs2.fc[h2.nonsign.fc]  <- 0
+		}
+		
+		simulation1.fc <- matrix(data=cbind(orthologs.fc, homeologs1.fc), ncol=2)
+		
+		if(second.hybrid == TRUE){
+			simulation2.fc <- matrix(data=cbind(orthologs.fc, homeologs2.fc), ncol=2)
+		}
 		
 		# calculate ortholog-homeolog expression classes
-		outcome    <- apply(simulation, 1, classify)
-		outcome.fc <- apply(simulation.fc, 1, classify)
+		outcome1    <- apply(simulation1, 1, classify)
+		outcome1.fc <- apply(simulation1.fc, 1, classify)
+		
+		if(second.hybrid == TRUE){
+			outcome2    <- apply(simulation2, 1, classify)
+			outcome2.fc <- apply(simulation2.fc, 1, classify)		
+		}
 		
 		# summarize results
-		this.res[i,] <- c( conditions, sim.nde, sim.nde.fc, count(outcome), count(outcome.fc) )	
+		if(second.hybrid == FALSE){
+			this.res[i,] <- c( conditions, sim.nde, sim.nde.fc, count(outcome1), count(outcome1.fc) )
+		}else if(second.hybrid == TRUE){
+			this.res[i,] <- c( conditions, sim.nde, sim.nde.fc, count(outcome1), count(outcome1.fc), count(outcome2), count(outcome2.fc) )
+		}
+			
 	}
 	
 	return( this.res )
